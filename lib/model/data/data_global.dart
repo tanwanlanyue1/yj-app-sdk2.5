4a57a55e09c892d447c5b4e46d03912f9859a67c
maizj <1064276001@qq.com>
2021-06-30 14:32:57 +0800

    更改了path_provider的方法，适配ios

--- lib/model/data/data_global.dart
+++ lib/model/data/data_global.dart
@@ -6,6 +6,7 @@ import 'package:scet_app/model/provider/provider_app.dart';
 import 'package:scet_app/utils/api/Request.dart';
 import 'package:scet_app/utils/tool/storage/data_storageKey.dart';
 import 'package:scet_app/utils/tool/storage/storage.dart';
+
 /// 全局配置
 class Global {
   /// 用户配置
@@ -28,12 +29,12 @@ class Global {
   static bool get isRelease => bool.fromEnvironment("dart.vm.product");
 
   /// app文件本地保存路径
-  static String appDownload ='';
+  static String appDownload = '';
 
   /// 三个字体文件路径
-  static String fontFileM ='';
-  static String fontFileB ='';
-  static String fontFileR ='';
+  static String fontFileM = '';
+  static String fontFileB = '';
+  static String fontFileR = '';
 
   ///全局的navigatorKey
   static GlobalKey<NavigatorState> navigatorKey = GlobalKey();
@@ -43,11 +44,12 @@ class Global {
     // 运行初始
     WidgetsFlutterBinding.ensureInitialized();
 
-     appDownload = (await getExternalStorageDirectory()).path + "/fontDownload/";
+    appDownload =
+        (await getApplicationDocumentsDirectory()).path + "/fontDownload/";
 
-     fontFileM = appDownload + StorageKey.FontMedium;
-     fontFileB = appDownload + StorageKey.FontBold;
-     fontFileR = appDownload + StorageKey.FontRegular;
+    fontFileM = appDownload + StorageKey.FontMedium;
+    fontFileB = appDownload + StorageKey.FontBold;
+    fontFileR = appDownload + StorageKey.FontRegular;
 
     // 本地存储工具初始化
     await StorageUtil.init();
@@ -56,16 +58,17 @@ class Global {
     Request();
 
     // 读取设备是否已经打开过
-    isFirstOpen = StorageUtil().getBool(StorageKey.STORAGE_DEVICE_ALREADY_OPEN_KEY);
+    isFirstOpen =
+        StorageUtil().getBool(StorageKey.STORAGE_DEVICE_ALREADY_OPEN_KEY);
     if (isFirstOpen) {
-      await readFont(fontFileM,"M");
-      await readFont(fontFileB,"B");
-      await readFont(fontFileR,"R");
+      await readFont(fontFileM, "M");
+      await readFont(fontFileB, "B");
+      await readFont(fontFileR, "R");
     }
 
-     //读取离线用户token
+    //读取离线用户token
     var _token = StorageUtil().getString(StorageKey.Token);
-    if (_token != null && _token != 'null'  &&  _token != '') {
+    if (_token != null && _token != 'null' && _token != '') {
       token = _token;
       isOfflineLogin = true;
     }
@@ -73,13 +76,13 @@ class Global {
     // android 状态栏为透明的沉浸
     if (Platform.isAndroid) {
       SystemUiOverlayStyle systemUiOverlayStyle =
-      SystemUiOverlayStyle(statusBarColor: Colors.transparent);
+          SystemUiOverlayStyle(statusBarColor: Colors.transparent);
       SystemChrome.setSystemUIOverlayStyle(systemUiOverlayStyle);
     }
   }
 
   ///  加载字体的方法
-  static Future<void> readFont(String path,String name) async {
+  static Future<void> readFont(String path, String name) async {
     print('文件 $path --->读取成功');
     var fontLoader = FontLoader(name);
     fontLoader.addFont(getCustomFont(path));
